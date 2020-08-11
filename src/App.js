9f40082f576c12c8cbe2df1a1f9e033d29159aa5
Salman Khurshid <salmankhurshid.me@gmail.com>
2020-08-11 23:21:59 +0530

    Completed the calculator

--- src/App.js
+++ src/App.js
@@ -1,24 +1,107 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { evaluate } from 'mathjs';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState('0');
+
+  const handleClick = (e) => {
+    const { value } = e.target;
+    switch (value) {
+      case 'clear':
+        setData('0');
+        break;
+      default:
+        setData(data + value);
+        break;
+    }
+  };
+
+  const solve = () => {
+    let answer = evaluate(data);
+    setData(answer);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // can use custom button for code reusability
+    // but we have to type as many times anyway
+    <div className='App'>
+      <h1>Simple Calculator</h1>
+
+      <div className='container'>
+        <div id='display' className='row'>
+          <p id='display-number'>{data}</p>
+        </div>
+
+        <div className='row'>
+          <button id='clear' onClick={handleClick} value='clear'>
+            CLEAR
+          </button>
+          <button id='sign' value='+/-'>
+            +/-
+          </button>
+          <button id='divide' onClick={handleClick} value='/'>
+            ÷
+          </button>
+        </div>
+
+        <div className='row'>
+          <button id='num7' onClick={handleClick} value='7'>
+            7
+          </button>
+          <button id='num8' onClick={handleClick} value='8'>
+            8
+          </button>
+          <button id='num9' onClick={handleClick} value='9'>
+            9
+          </button>
+          <button id='multiply' onClick={handleClick} value='*'>
+            ×
+          </button>
+        </div>
+
+        <div className='row'>
+          <button id='num4' onClick={handleClick} value='4'>
+            4
+          </button>
+          <button id='num5' onClick={handleClick} value='5'>
+            5
+          </button>
+          <button id='num6' onClick={handleClick} value='6'>
+            6
+          </button>
+          <button id='subtract' onClick={handleClick} value='-'>
+            -
+          </button>
+        </div>
+
+        <div className='row'>
+          <button id='num1' onClick={handleClick} value='1'>
+            1
+          </button>
+          <button id='num2' onClick={handleClick} value='2'>
+            2
+          </button>
+          <button id='num3' onClick={handleClick} value='3'>
+            3
+          </button>
+          <button id='add' onClick={handleClick} value='+'>
+            +
+          </button>
+        </div>
+
+        <div className='row'>
+          <button id='num0' onClick={handleClick} value='0'>
+            0
+          </button>
+          <button id='decimal' onClick={handleClick} value='.'>
+            .
+          </button>
+          <button id='solve' onClick={solve} value='='>
+            =
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
